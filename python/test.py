b5c61158e9c696cca855a577112460901104ae42
Maxime MICHON <mmi@cab.local>
2020-10-01 15:28:25 +0200

    vérif qvw2

--- python/test.py
+++ python/test.py
@@ -1,3 +1,5 @@
-import tkinter, tkinter.messagebox
-tkinter.Tk().withdraw()
-tkinter.messagebox.showwarning(title="bruh", message="bruhmode")
+#import tkinter, tkinter.messagebox
+#tkinter.Tk().withdraw()
+#tkinter.messagebox.showwarning(title="bruh", message="bruhmode")
+filesinfo = [1, 2, 3]
+print(filesinfo)
---
+++ python/verifqvw_v2.py
@@ -0,0 +1,27 @@
+import os, glob, time, datetime
+
+def my_function(input_dir):
+    filesinfo = [] #création d'une liste vide
+    today = datetime.datetime.today() #récupère la date du pc
+    os.chdir(input_dir) 
+    for fichiers in glob.glob("**", recursive=True): #récupère tout les fichiers 
+        modified_date = datetime.datetime.fromtimestamp(os.path.getmtime(fichiers))
+        path = os.path.abspath(fichiers)
+        duration = today - modified_date
+        if duration.days > 1 and duration.days < 360:
+            filesinfo.append(f"{modified_date} = {path}")
+    writefile(filesinfo)
+            
+def writefile(filesinfo):
+    todayday = datetime.datetime.today().date()
+    output_name = f"//10.2.30.61/c$/temp/{todayday}.txt"
+    with open(output_name,"a+") as f:
+        for item in filesinfo:
+            f.write(item + "\n\n")
+# liste de liste mettre l'appel des fonctions dans une liste
+my_function("//10.2.30.61/c$/Qlikview_Tropal/Apps/")
+my_function("//10.2.30.61/c$/Qlikview_Tropal_Paie/Apps/")
+my_function("//10.2.30.61/c$/Qlikview_Jastres/apps/")
+my_function("//10.2.30.61/c$/Qlikview_Compta/SuiviCompta/apps/")
+exit
+
