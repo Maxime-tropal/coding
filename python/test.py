871452962d264af72e8cc73bd1093c294d8ba776
Maxime MICHON <mmi@cab.local>
2020-10-02 15:29:41 +0200

    Update test.py

--- python/test.py
+++ python/test.py
@@ -1,5 +1,21 @@
-#import tkinter, tkinter.messagebox
-#tkinter.Tk().withdraw()
-#tkinter.messagebox.showwarning(title="bruh", message="bruhmode")
-filesinfo = [1, 2, 3]
-print(filesinfo)
+from tkinter import * 
+
+test1 = Tk()
+
+topFrame = Frame(test1)
+topFrame.pack()
+bottomFrame = Frame(test1)
+bottomFrame.pack(side=BOTTOM)
+
+button1 = Button(topFrame, text="Click me", fg="red")
+button2 = Button(topFrame, text="No click me", fg="blue")
+button3 = Button(topFrame, text="Click me !!", fg="green")
+button4 = Button(bottomFrame, text="CLICK", fg="purple")
+
+button1.pack(side=LEFT)
+button2.pack(side=RIGHT)
+button3.pack(side=RIGHT)
+button4.pack(side=BOTTOM)
+
+
+test1.mainloop()
