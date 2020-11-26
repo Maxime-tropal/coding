06febe43f3e3e2067fa5d21ff3d1431dd9dfcd2a
Maxime MICHON <mmi@cab.local>
2020-11-26 13:24:50 +0100

    ajout warnings

--- python/Generateur_pesee.py
+++ python/Generateur_pesee.py
@@ -1,4 +1,4 @@
-import os, glob, time
+import os, glob, time, ctypes, pathlib
 
 liste= []
 listecsv = []
@@ -16,7 +16,22 @@ def nbtxt(input_dir):
         nomtexte = ftextes
         return nomtexte
 
+def verif():
+    csv = pathlib.Path("\\\\vifprod1\\ascii\\abattage\\elisa\\brive\\reference.csv")
+    txt = pathlib.Path("\\\\vifprod1\\ascii\\abattage\\elisa\\brive\\" + str(nomtexte))
+    if csv.exists():
+        pass
+    else:
+        ctypes.windll.user32.MessageBoxW(0, "Le fichier de référence n'existe pas !", "Attention", 1)
+        quit
+    if txt.exists():
+        pass
+    else:
+        ctypes.windll.user32.MessageBoxW(0, "Le fichier texte n'est pas dans le répertoire", "Attention", 1)
+        quit
+
 nomtexte = nbtxt(chemintxt)
+verif()
 
 with open(cheminref,"r") as f:
     for line in f:
@@ -34,7 +49,6 @@ with open(chemintxt + nomtexte ,"r") as f:
     # string.find permet de compter le nb d'occurence. if str.find = -1 signifie 'si tu ne trouves aucune occurence' et !=-1 si tu trouves au moins 1
     # occurence dans la string
     # donc ici: champs.find permet de voir si il trouve une valeur identique à une liste de réf, et si oui la remplace par une valeur.
-
         for a,b in zip(ccourt,nvccourt):    
             if champs.find(a) !=-1:
                 liste[7]=b
@@ -42,6 +56,6 @@ with open(chemintxt + nomtexte ,"r") as f:
         listefinale.append(ligne)
 
 os.rename(chemintxt + nomtexte, archive + nomtexte )
-with open("\\\\vifprod1\\ascii\\abattage\\elisa\\brive\\pesée_" + nomtexte,"w") as f:
+with open("\\\\vifprod1\\ascii\\abattage\\elisa\\brive\\pesée.txt","w") as f:
     for element in listefinale:
         f.write(element)
