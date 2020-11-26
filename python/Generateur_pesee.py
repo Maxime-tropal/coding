a4e3530212b71c9a37646bc4af4fe5b2afa040d6
Maxime MICHON <mmi@cab.local>
2020-11-26 10:28:08 +0100

    MAJ brive
    ajout détection automatique des chemins de la reference et du fichier texte + déplacement automatique du fichier texte après execution du script dans le répertoir archive

--- python/Generateur_pesee.py
+++ python/Generateur_pesee.py
@@ -1,18 +1,33 @@
+import os, glob, time
+
 liste= []
 listecsv = []
 listefinale = []
 ccourt=[]
 nvccourt=[]
-with open("C:\\Users\\mmi\\Desktop\\Classeur1.csv","r") as f:
+cheminref = "\\\\vifprod1\\ascii\\abattage\\elisa\\brive\\reference.csv"
+chemintxt = "\\\\vifprod1\\ascii\\abattage\\elisa\\brive\\"
+archive = "\\\\vifprod1\\ascii\\abattage\\elisa\\brive\\archive\\"
+
+def nbtxt(input_dir):
+    os.chdir(input_dir)
+    #scan du répertoire \\brive en ne prenant que les fichiers commençant par e_xxxxxx.
+    for ftextes in glob.glob("e_*"):
+        nomtexte = ftextes
+        return nomtexte
+
+nomtexte = nbtxt(chemintxt)
+
+with open(cheminref,"r") as f:
     for line in f:
         listecsv = line.split(";")
+        # liste.strip() enlève le dernier bout de chaque item de la liste a savoir le \n ici
         listecsv[-1] = listecsv[-1].strip()
-        print(listecsv)
         #séparation des 2 colonnes ccourt et nvccourt en 2 listes différentes
         ccourt.append(listecsv[0])
         nvccourt.append(listecsv[2])
 
-with open("C:\\Users\\mmi\\Desktop\\e_20111031.txt","r") as f: 
+with open(chemintxt + nomtexte ,"r") as f: 
     for line in f:
         liste = line.split(";")
         champs = liste[7]
@@ -26,9 +41,7 @@ with open("C:\\Users\\mmi\\Desktop\\e_20111031.txt","r") as f:
         ligne = ';'.join(liste)
         listefinale.append(ligne)
 
-
-
-with open("C:\\Users\\mmi\\Desktop\\pesée.txt","w") as f:
+os.rename(chemintxt + nomtexte, archive + nomtexte )
+with open("\\\\vifprod1\\ascii\\abattage\\elisa\\brive\\pesée_" + nomtexte,"w") as f:
     for element in listefinale:
         f.write(element)
-    
