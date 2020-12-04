b6db138a08a58eda114f0353878a5adc33d9e9ac
Maxime-tropal <71068056+Maxime-tropal@users.noreply.github.com>
2020-12-04 14:02:21 +0100

    test brive pas concluant + test bl metro
    metro -> extension compliquée a lire

--- python/tropal/Generateur_pesee_test.py
+++ python/tropal/Generateur_pesee_test.py
@@ -17,6 +17,7 @@ def nbtxt(input_dir):
         return nomtexte
 
 def verif():
+
     csv = pathlib.Path("\\\\vifprod1\\ascii\\abattage\\elisa\\brive\\reference.csv")
     txt = pathlib.Path("\\\\vifprod1\\ascii\\abattage\\elisa\\brive\\" + str(nomtexte))
     if csv.exists():
@@ -30,7 +31,7 @@ def verif():
         ctypes.windll.user32.MessageBoxW(0, "Le fichier texte n'est pas dans le répertoire", "Attention", 1)
         exit()
     
-def lecture():
+
     with open(cheminref,"r") as f:
         for line in f:
             listecsv = line.split(";")
@@ -49,12 +50,33 @@ def lecture():
         # occurence dans la string
         # donc ici: champs.find permet de voir si il trouve une valeur identique à une liste de réf, et si oui la remplace par une valeur.
             for a,b in zip(ccourt,nvccourt):    
-                if champs.find(a):
-                    liste[7]=b
+                if champs.find(a) !=1:
+                    liste[7] = b
                     print(a)
-                elif champs.find(a) not in str(liste[7]):
-                    ctypes.windll.user32.MessageBoxW(0, "Ces fournisseurs ne sont pas dans le fichier de référence: " + liste[7], "Attention", 1)
-                    
+#elif champs.find(a) not in str(liste[7]):
+                    #ctypes.windll.user32.MessageBoxW(0, "Ces fournisseurs ne sont pas dans le fichier de référence: " + liste[7], "Attention", 1)
+                           
+    
+def lecture():
+    with open(cheminref,"r") as f:
+        for line in f:
+            listecsv = line.split(";")
+            # liste.strip() enlève le dernier bout de chaque item de la liste a savoir le \n ici
+            listecsv[-1] = listecsv[-1].strip()
+            #séparation des 2 colonnes ccourt et nvccourt en 2 listes différentes
+            ccourt.append(listecsv[0])
+            nvccourt.append(listecsv[2])
+
+    with open(chemintxt + nomtexte ,"r") as f: 
+        for line in f:
+            liste = line.split(";")
+            champs = liste[7]
+        # string.find permet de compter le nb d'occurence. if str.find = -1 signifie 'si tu ne trouves aucune occurence' et !=-1 si tu trouves au moins 1
+        # occurence dans la string
+        # donc ici: champs.find permet de voir si il trouve une valeur identique à une liste de réf, et si oui la remplace par une valeur.
+            for a,b in zip(ccourt,nvccourt):    
+                if champs.find(a) !=-1:
+                    liste[7]=b
             ligne = ';'.join(liste)
             listefinale.append(ligne)
 
--- python/tropal/test_metro.py
+++ python/tropal/test_metro.py
@@ -7,14 +7,16 @@ def nbbltoday(cheminbl):
     listebl = []  # création d'une liste vide
     today = datetime.datetime.today()  # récupère la date du pc
     os.chdir(cheminbl)
-    for fichiers in glob.glob("**", recursive=True): # récupère tout les fichiers dans le dossier
+    for fichiers in glob.glob("**"): # récupère tout les fichiers dans le dossier
         modified_date = datetime.datetime.fromtimestamp(
                 os.path.getmtime(fichiers))
         path = os.path.abspath(fichiers)
         duration = today - modified_date
-        if duration.seconds > 43200 and duration.days < 5:
-            filesinfo.append(f"{modified_date} = {path}")
-    #writefile(filesinfo, output_name)
-            print(filesinfo)
-
+        if duration.days < 1:
+            listebl.append(f"{path}")
+    for item in listebl:
+        bl = os.path.basename(item)
+    with open(bl) as f:
+        a = f.readlines()
+        print(a)
 nbbltoday(cheminbl)
