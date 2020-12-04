8fa0981a681d2e90d90d5d1c9080ef819b4b92ca
Maxime-tropal <71068056+Maxime-tropal@users.noreply.github.com>
2020-12-04 09:07:54 +0100

    Update test_metro.py

--- python/tropal/test_metro.py
+++ python/tropal/test_metro.py
@@ -1,10 +1,20 @@
-import os,glob,time
+import os, glob, time, pathlib, datetime
 
 cheminbl = "\\\\srvbureautique\\tropalcommun\\informatique\\metro\\bl"
 cheminretourmetro = "\\\\srvbureautique\\tropalcommun\\informatique\\metro\\retour metro"
 
-listebl = []
-today1 = datetime.datetime.today().date()
+def nbbltoday(cheminbl):
+    listebl = []  # création d'une liste vide
+    today = datetime.datetime.today()  # récupère la date du pc
+    os.chdir(cheminbl)
+    for fichiers in glob.glob("**", recursive=True): # récupère tout les fichiers dans le dossier
+        modified_date = datetime.datetime.fromtimestamp(
+                os.path.getmtime(fichiers))
+        path = os.path.abspath(fichiers)
+        duration = today - modified_date
+        if duration.seconds > 43200 and duration.days < 5:
+            filesinfo.append(f"{modified_date} = {path}")
+    #writefile(filesinfo, output_name)
+            print(filesinfo)
 
-def nbbltoday(input_dir):
-    
+nbbltoday(cheminbl)
