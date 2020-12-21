ba76457c0ea1a6116024d3cde68d16e0beeacc54
Maxime-tropal <71068056+Maxime-tropal@users.noreply.github.com>
2020-12-21 08:48:30 +0100

    modif

--- python/tropal/Generateur_pesee_test.py
+++ python/tropal/Generateur_pesee_test.py
@@ -17,7 +17,7 @@ def nbtxt(input_dir):
         return nomtexte
 
 def verif():
-
+    liste_verif = []
     csv = pathlib.Path("\\\\vifprod1\\ascii\\abattage\\elisa\\brive\\reference.csv")
     txt = pathlib.Path("\\\\vifprod1\\ascii\\abattage\\elisa\\brive\\" + str(nomtexte))
     if csv.exists():
@@ -30,8 +30,12 @@ def verif():
     else:
         ctypes.windll.user32.MessageBoxW(0, "Le fichier texte n'est pas dans le répertoire", "Attention", 1)
         exit()
+    with open(cheminref, "r") as f:
+        for lines in f:
+            liste_verif = lines.split(";")
+            print(liste_verif)
     
-
+def lecture():
     with open(cheminref,"r") as f:
         for line in f:
             listecsv = line.split(";")
@@ -49,45 +53,13 @@ def verif():
         # string.find permet de compter le nb d'occurence. if str.find = -1 signifie 'si tu ne trouves aucune occurence' et !=-1 si tu trouves au moins 1
         # occurence dans la string
         # donc ici: champs.find permet de voir si il trouve une valeur identique à une liste de réf, et si oui la remplace par une valeur.
-            #while True:
-                #try:
-            print("toto)")
-            temp = str()
-            for a,b in zip(ccourt,nvccourt):
-                if champs.find(a) != -1:
-                    temp = b
-                    print(temp)
-            return (temp)
-          #  print("---"+temp)
-           # if len(temp) >0:
-            #    liste[7]= temp
-                #print(temp)
-            #else:
-                #pass
-                #ctypes.windll.user32.MessageBoxW(0, "Ces fournisseurs ne sont pas dans le fichier de référence: " + liste[7], "Attention", 1)
-                #print("anomalie:" + temp + str(len(temp)))
-                           
-    
-def lecture():
-    with open(cheminref,"r") as f:
-        for line in f:
-            listecsv = line.split(";")
-            # liste.strip() enlève le dernier bout de chaque item de la liste a savoir le \n ici
-            listecsv[-1] = listecsv[-1].strip()
-            #séparation des 2 colonnes ccourt et nvccourt en 2 listes différentes
-            ccourt.append(listecsv[0])
-            nvccourt.append(listecsv[2])
-
-    with open(chemintxt + nomtexte ,"r") as f: 
-        for line in f:
-            liste = line.split(";")
-            champs = liste[7]
-        # string.find permet de compter le nb d'occurence. if str.find = -1 signifie 'si tu ne trouves aucune occurence' et !=-1 si tu trouves au moins 1
-        # occurence dans la string
-        # donc ici: champs.find permet de voir si il trouve une valeur identique à une liste de réf, et si oui la remplace par une valeur.
             for a,b in zip(ccourt,nvccourt):    
-                if champs.find(a) !=-1:
+                if champs.find(a):
                     liste[7]=b
+                    print(a)
+                #elif champs.find(a) not in str(liste[7]):
+                #    ctypes.windll.user32.MessageBoxW(0, "Ces fournisseurs ne sont pas dans le fichier de référence: " + liste[7], "Attention", 1)
+                    
             ligne = ';'.join(liste)
             listefinale.append(ligne)
 
@@ -99,6 +71,5 @@ def ecriture():
 
 nomtexte = nbtxt(chemintxt)
 verif()
-toto = lecture()
-print("---"+ str(toto)
+lecture()
 #ecriture()
