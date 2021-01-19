53dd90fdd3285b33d3f1303e9ae8266ed2465625
Maxime MICHON <mmi@cab.local>
2021-01-19 08:30:38 +0100

    maj en cours
    test de dl depuis python

--- python/tropal/Trello_export.py
+++ python/tropal/Trello_export.py
@@ -1,34 +1,22 @@
-import os, time, pyperclip
+import os, time, requests
 from tkinter import Label, Button, Tk
 
 
-os.chdir("C:\\Users\\mmi\\Desktop\\DEV\\python\\tropal")
-os.startfile("Trello.bat")
+os.chdir("C:\\Users\\mmi\\Documents\\GitHub\\coding\\python\\tropal")
+#os.startfile("Trello.bat")
 time.sleep(5)
-with open("lien_telechargement.txt", "r") as f:
+with open("lien_trello.txt", "r") as f:
     result = []
     for lines in f:
         result = lines.split("\":\"")
         
 
 lien_dl = str([result[-1]])
-lien_dl = lien_dl[:-4]
-print(lien_dl)
+lien_dl = lien_dl[2:-6]
 
+lien_dl_final = "https://trello.com" + lien_dl
 
-def copylink(fenetre):
-    pyperclip.copy("https://trello.com/" + lien_dl)
-    pyperclip.paste()
-    label1 = Label(text="Lien copié dans le presse papier !")
-    label1.pack()
+print(lien_dl_final)
 
-root = Tk()
-root.geometry("300x100")
-
-
-
-btn1= Button(root,text="Copier le lien de téléchargement", command=lambda: copylink(root))
-btn1.pack()
-
-root.mainloop()
-    
+response = requests.get(lien_dl_final, verify=False, auth=("34fc8c60f8adc6a5065b3164efd629e0", "271bce676bf1a7a040f05058d412dae83144c75a2ec23cf428b9bab178d15e69"))
+print(response.text)
